3be9ce8d79ab0956aadbc314fab8a03cb2489a7e
Wesley1105 <wesley.sousa28@etec.sp.gov.br>
2020-02-27 12:25:14 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+void main()=> runApp(
+  MaterialApp(
+    home: Center(
+
+      child: Container(
+          color: Colors.blue,
+          child: Text('Hello World!!!', style: TextStyle(color: Colors.white))
+
+      ),
+    ),
+  ),
+);
